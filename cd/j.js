9ed5f42e6cea68b99dd8046a6abff6acfb13694d
Tyoma0 <artyom0070071@mail.ru>
2024-09-21 15:03:04 +0300

    f

--- cd/j.js
+++ cd/j.js
@@ -1,7 +1,42 @@
-function sumMix(x){
-    var somme = 0;
-    for (i = 0; i<x.length; i++) {
-      somme += Number(x[i]);
-    }
-    return somme;
-  }
+
+
+// Если дан массив целых чисел в виде строк и чисел, возвращается сумма значений массива, как если бы все они были числами.
+
+// Верните свой ответ в виде числа.
+
+
+// function sumMix(x){
+//     var somme = 0;
+//     for (i = 0; i<x.length; i++) {
+//       somme += Number(x[i]);
+//     }
+//     return somme;
+//   }
+
+
+
+
+
+
+
+// При наличии массива целых чисел решение должно найти наименьшее целое число.
+
+// Например:
+
+// При условии, что ваше решение вернется [34, 15, 88, 2]2
+// При условии, что ваше решение вернется [34, -345, -1, 100]-345
+// Для целей этого ката вы можете предположить, что предоставленный массив не будет пустым.
+
+
+
+// function findSmallestInt(arr) {
+//   let smallest = arr[0]
+//   for(let element of arr){
+//     if (element < smallest) {
+            
+//             smallest = element;
+//         }
+//   }
+//   return smallest
+//  }
+// console.log(findSmallestInt([5,6,4,3,8]) )
