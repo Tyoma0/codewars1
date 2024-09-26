65f64a108f1952f3ab43cc09feb04c07d3a3dea5
Tyoma0 <artyom0070071@mail.ru>
2024-09-26 22:07:54 +0300

    h

--- ja.js
+++ ja.js
@@ -413,33 +413,33 @@
 // 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 // */
 
-const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+// const WEB_TECH_IMAGES = [
+//   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+//   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+//   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+// ]
 
-const image = document.querySelector('img');
-const prevButton = document.getElementById('prev-button');
-const nextButton = document.getElementById('next-button');
+// const image = document.querySelector('img');
+// const prevButton = document.getElementById('prev-button');
+// const nextButton = document.getElementById('next-button');
 
-let currentImageIndex = 0;
+// let currentImageIndex = 0;
 
-function showImage() {
-  image.src = WEB_TECH_IMAGES[currentImageIndex];
-}
+// function showImage() {
+//   image.src = WEB_TECH_IMAGES[currentImageIndex];
+// }
 
-function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % WEB_TECH_IMAGES.length;
-  showImage();
-}
+// function nextImage() {
+//   currentImageIndex = (currentImageIndex + 1) % WEB_TECH_IMAGES.length;
+//   showImage();
+// }
 
-function prevImage() {
-  currentImageIndex = (currentImageIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
-  showImage();
-}
+// function prevImage() {
+//   currentImageIndex = (currentImageIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+//   showImage();
+// }
 
-nextButton.addEventListener('click', nextImage);
-prevButton.addEventListener('click', prevImage);
+// nextButton.addEventListener('click', nextImage);
+// prevButton.addEventListener('click', prevImage);
 
-showImage()
+// showImage()
